e1bf273f3b08cf3e1c7d1d00cba976f08159fd01
Eva Wiedmann <evawiedmann@gmail.com>
2019-11-23 14:18:31 -0800

    struggle with API functionality, testing, take lunch + walk

--- src/main.js
+++ src/main.js
@@ -5,6 +5,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 // API Framework
+(async () => {
+  let doctor = new Doctor();
+  const response = await marsphoto.getDoctor();
+  getElements(response);
+})();
+
+const getElements = function(response) {
+  $('#marsPhotos').attr("src", response.photos[100].img_src);
+};
+
+
 
 
 'No docotors fit your request'
